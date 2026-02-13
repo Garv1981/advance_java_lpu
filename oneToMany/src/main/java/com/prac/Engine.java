ecca7c19e87a61ec5bd2ae9a0b75556e2d23ace0
Garv Gakkhar <garvgakkhar014@gmail.com>
2026-02-13 12:13:50 +0530

    onetoone

--- oneToMany/src/main/java/com/prac/Engine.java
+++ oneToMany/src/main/java/com/prac/Engine.java
@@ -1,5 +1,61 @@
 package com.prac;
 
+import javax.persistence.*;
+
+@Entity
 public class Engine {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String type;
+    private String fuelType;
+    private double mileage;
+    private int cc;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine [id=" + id +
+                ", type=" + type +
+                ", fuelType=" + fuelType +
+                ", mileage=" + mileage +
+                ", cc=" + cc + "]";
+    }
 }
