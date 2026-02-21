e4a9b46cedfe400fc5caf842c61ab39ac764c173
Garv Gakkhar <garvgakkhar014@gmail.com>
2026-02-21 16:41:29 +0530

    updated till 21

--- CRM_Management/src/main/java/com/prac/Address.java
+++ CRM_Management/src/main/java/com/prac/Address.java
@@ -1,5 +1,39 @@
 package com.prac;
+import javax.persistence.*;
 
+@Entity
 public class Address {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long address_id;
+	private String city;
+	private String state;
+	private String pincode;
+	
+	public Address(long address_id, String city, String state, String pincode) {
+		this.address_id = address_id;
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+	}
+
+	public long getAddress_id() {
+		return address_id;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getPincode() {
+		return pincode;
+	}
+
+	
 }
--- CRM_Management/src/main/java/com/prac/Customer.java
+++ CRM_Management/src/main/java/com/prac/Customer.java
@@ -1,5 +1,50 @@
 package com.prac;
+import javax.persistence.*;
 
+@Entity
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	private String phone;
+	
+	
+	public Customer(int id, String name, String email, String phone) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+	
+	@Override
+	public String toString() {
+		return id + " "+name+" "+email+" "+phone;
+	}
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Address address;
+	
+	
+	
+	
 }
